6ed3f947e8c5668f426a817b6778497deb54a099
Thomas Standing <tomcoap@gmail.com>
2017-04-01 16:38:31 -0400

    add styling to event cards

--- react-slingshot/src/components/EventResult/EventResult.js
+++ react-slingshot/src/components/EventResult/EventResult.js
@@ -18,7 +18,7 @@ class EventResult extends Component {
       },
       events: [
         {
-          image: "dummy.jpg",
+          image: '',
           time: '',
           title: 'Please Wait While We Get Your Results',
           description: 'Thank you for your patience',
@@ -31,7 +31,7 @@ class EventResult extends Component {
   }
 
   componentDidMount() {
-      fetch(`http://localhost:8000/events/?location=${localStorage.city}`, {
+      fetch(`http://localhost:8000/events/?keywords=music&location=${localStorage.city}`, {
         method: 'GET'
       })
       .then(r => r.json())
@@ -55,31 +55,13 @@ class EventResult extends Component {
     });
   }
 
-  selectNewEvent() {
-    this.setState({isVisible: {opacity: '0'}});
-    this.setState({loader: {display: 'block'}});
-
-    fetch(`http://localhost:8000/events`, {
-      method: 'GET'
-    })
-    .then((results) => {
-      results.json().then((data) => {
-        this.setState({events: data[Math.floor(Math.random() * data.length)]});
-        this.setState({loader: {display: 'none'}});
-        this.setState({isVisible: {opacity: '1'}});
-      });
-    })
-    .catch((err) => {
-      console.log('ERROR: ', err);
-    })
-  }
-
   handleSubmit(event) {
-    fetch(`http://localhost:8000/events/?location=${localStorage.city}`, {
+    fetch(`http://localhost:8000/events/?keywords=music&location=${localStorage.city}`, {
         method: 'GET'
       })
       .then(r => r.json())
       .then(response => {
+        this.setState({loader: {display: 'none'}});
         const events = response.results.search.events.event.map((event) => {
           return {
             image: event.image.url || 'https://d30y9cdsu7xlg0.cloudfront.net/png/34527-200.png',
@@ -91,8 +73,8 @@ class EventResult extends Component {
             url: event.venue_url
           };
         });
-
         this.setState({ events });
+        this.setState({isVisible: {opacity: '1'}});
       })
       .catch((err) => {
         console.log('ERROR: ', err);
@@ -105,50 +87,51 @@ class EventResult extends Component {
       <div>
         <Nav />
         <div className="container">
-          <div className="event-results">
-            <div className="event-card">
-              <EventInfo
-                image={this.state.events[0].image}
-                time={this.state.events[0].time}
-                title={this.state.events[0].title}
-                description={this.state.events[0].description}
-                venue={this.state.events[0].venue}
-                address={this.state.events[0].address}
-                url={this.state.events[0].url}
-              />
-            </div>
+          <div className="loading-anim" style={this.state.loader}></div>
+            <div className="event-results">
+              <div className="event-card">
+                <EventInfo
+                  image={this.state.events[0].image}
+                  time={this.state.events[0].time}
+                  title={this.state.events[0].title}
+                  description={this.state.events[0].description}
+                  venue={this.state.events[0].venue}
+                  address={this.state.events[0].address}
+                  url={this.state.events[0].url}
+                />
+              </div>
 
-             <div className="event-card">
-              <EventInfo
-                image={this.state.events[1].image}
-                time={this.state.events[1].time}
-                title={this.state.events[1].title}
-                description={this.state.events[1].description}
-                venue={this.state.events[1].venue}
-                address={this.state.events[1].address}
-                url={this.state.events[1].url}
-              />
-            </div>
+               <div className="event-card">
+                <EventInfo
+                  image={this.state.events[1].image}
+                  time={this.state.events[1].time}
+                  title={this.state.events[1].title}
+                  description={this.state.events[1].description}
+                  venue={this.state.events[1].venue}
+                  address={this.state.events[1].address}
+                  url={this.state.events[1].url}
+                />
+              </div>
 
-             <div className="event-card">
-              <EventInfo
-                image={this.state.events[2].image}
-                time={this.state.events[2].time}
-                title={this.state.events[2].title}
-                description={this.state.events[2].description}
-                venue={this.state.events[2].venue}
-                address={this.state.events[2].address}
-                url={this.state.events[2].url}
-              />
-            </div>
+               <div className="event-card">
+                <EventInfo
+                  image={this.state.events[2].image}
+                  time={this.state.events[2].time}
+                  title={this.state.events[2].title}
+                  description={this.state.events[2].description}
+                  venue={this.state.events[2].venue}
+                  address={this.state.events[2].address}
+                  url={this.state.events[2].url}
+                />
+              </div>
 
-            <div className="randomizer">
-              <h2 id="picky">Feeling Picky?</h2>
-              <button className="standard-btn" onClick={this.handleSubmit.bind(this)}>Pick Again</button>
+              <div className="randomizer">
+                <h2 id="picky">Feeling Picky?</h2>
+                <button className="standard-btn" onClick={this.handleSubmit.bind(this)}>Pick Again!</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
     )
   }
 }
