5913f5ab450c3bc97f246306b01c7ac3022be4c9
Thomas Standing <tomcoap@gmail.com>
2017-04-01 14:11:21 -0400

    fixed button on home page

--- react-slingshot/src/components/EventResult/EventResult.js
+++ react-slingshot/src/components/EventResult/EventResult.js
@@ -75,29 +75,28 @@ class EventResult extends Component {
   }
 
   handleSubmit(event) {
-    // event.preventDefault();
+    fetch(`http://localhost:8000/events/?location=${localStorage.city}`, {
+        method: 'GET'
+      })
+      .then(r => r.json())
+      .then(response => {
+        const events = response.results.search.events.event.map((event) => {
+          return {
+            image: event.image.url || 'https://d30y9cdsu7xlg0.cloudfront.net/png/34527-200.png',
+            time: event.start_time,
+            title: event.title,
+            description: event.description,
+            venue: event.venue_name,
+            address: event.venue_address,
+            url: event.venue_url
+          };
+        });
 
-    // fetch(`http://localhost:8000/events/${this.state.value}`, {
-    //   method: 'POST',
-    //   body: JSON.stringify({
-    //     events: {
-    //       title: `${this.state.event.title}`,
-    //       description: `${this.state.event.description}`,
-    //       venue_name: `${this.state.event.venue_name}`,
-    //       venue_address: `${this.state.event.venue_address}`
-    //     }
-    //   }),
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   }
-    // })
-    // .then((data) => {
-    //   // browserHistory.push('/users/dashboard');
-    //   console.log(data);
-    // })
-    // .catch((err) => {
-    //   console.log(err);
-    // })
+        this.setState({ events });
+      })
+      .catch((err) => {
+        console.log('ERROR: ', err);
+    });
 
   }
 
@@ -145,7 +144,7 @@ class EventResult extends Component {
 
             <div className="randomizer">
               <h2 id="picky">Feeling Picky?</h2>
-              <button className="standard-btn" onClick={this.selectNewEvent.bind(this)}>Pick Again</button>
+              <button className="standard-btn" onClick={this.handleSubmit.bind(this)}>Pick Again</button>
             </div>
           </div>
         </div>
