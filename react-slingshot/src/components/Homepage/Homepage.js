68f4dac9c00944e2c17fe810732813196b7c562b
Thomas Standing <tomcoap@gmail.com>
2017-04-02 13:26:58 -0400

    npm install react-bootstrap

--- react-slingshot/src/components/Homepage/Homepage.js
+++ react-slingshot/src/components/Homepage/Homepage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Nav from "../Nav/Nav";
 import { browserHistory } from "react-router";
 
+
 class Homepage extends Component {
   constructor(props) {
     super(props);
